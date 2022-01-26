1d2386886c36cc801fa48344ca7ac072c0e0c0bf
AbdullahAbbas08 <34220789+AbdullahAbbas08@users.noreply.github.com>
2022-01-26 13:04:39 +0200

    handle 401 error

--- src/Interceptor/interceptors.ts
+++ src/Interceptor/interceptors.ts
@@ -1,6 +1,8 @@
 import { HTTP_INTERCEPTORS } from "@angular/common/http";
 import { ErrorHandleingInterceptor } from "./error-handleing.interceptor";
+import { HttpInterceptorInterceptor } from "./http-interceptor.interceptor";
 
 export const InterceptorsProvider = [
-    {provide:HTTP_INTERCEPTORS,useClass:ErrorHandleingInterceptor,multi:true}
+    {provide:HTTP_INTERCEPTORS,useClass:ErrorHandleingInterceptor,multi:true},
+    {provide:HTTP_INTERCEPTORS,useClass:HttpInterceptorInterceptor,multi:true}
 ]; 
