d04adbb70fe61e1e2422bc4f645dda13450e570b
abdullah415 <hager.mamdouh112@gmail.com>
2022-02-02 17:20:32 +0200

    edit emr profile

--- src/app/Components/main/Appointments/emr/EMR-Profile/emr-profile.component.ts
+++ src/app/Components/main/Appointments/emr/EMR-Profile/emr-profile.component.ts
@@ -1,8 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup } from '@angular/forms';
 import { ActivatedRoute } from '@angular/router';
+import { filter, map } from 'rxjs/operators';
 import { AppointmentService } from 'src/Service/Appointment/appointment.service';
 import { EmrService } from 'src/Service/emr/emr.service';
+import { LookupsService } from 'src/Service/Lockups/lookups.service';
 
 @Component({
   selector: 'app-emr-profile',
@@ -11,14 +13,21 @@ import { EmrService } from 'src/Service/emr/emr.service';
 })
 export class EmrProfileComponent implements OnInit {
   id;
+  data=[];
+  MedicalType ;
+  medicalImg;
+  profileHistory;
   profiledetails;
   EmrForm:FormGroup;
-  constructor(private route:ActivatedRoute , private emrService : EmrService) { 
+  constructor(private route:ActivatedRoute ,
+     private emrService : EmrService ,
+     private lookupService:LookupsService
+     ) { 
      this.route.paramMap.subscribe(param=>{
-      this.id=param.get('appointmentID');
-      this.getEmrProfile(this.id)
+      this.id=param.get('ID');
+      this.GetEmrHistory(this.id)
       // console.log(param.get('appointmentID'));
-      this.emrService.id.next(this.id) 
+      this.emrService.id.next(this.id)  
     })
   }
 
@@ -36,10 +45,45 @@ export class EmrProfileComponent implements OnInit {
     
   }
 
-  getEmrProfile(id){
-    this.emrService.GetEmrProfile(id).subscribe(res=>{
+  getMedicalTYpe(id){    
+    if(id==1){
+      this.MedicalType="Clinic Appointment"
+      this.medicalImg="../../../../../../assets/img/medical-type/location.png"
+    }
+    else if(id==2){
+      this.MedicalType= "Home visit"
+      this.medicalImg="../../../../../../assets/img/medical-type/location.png"
+    }
+    else if(id==3){
+      this.MedicalType=  "Video appointment"
+      this.medicalImg="../../../../../../assets/img/medical-type/location.png"
+    }
+    else if(id==4){
+      this.MedicalType= "Call appointment"
+      this.medicalImg="../../../../../../assets/img/medical-type/location.png"
+    }
+    else if(id==5){
+      this.MedicalType= "Chat appointment"
+      this.medicalImg="`../../../../../..`/assets/img/medical-type/location.png"
+    }
+  }
+
+  GetEmrHistory(id){
+    this.emrService.GetEmrHistory(id).subscribe(res=>{
+      this.profileHistory= res.Data;
+      console.log(this.profileHistory);
+      this.profileHistory.map(res=>{
+        this.GetEmrDetails(res.AppointmentId)
+        this.getMedicalTYpe(res.MedicalExaminationTypeId)
+      })
+    })
+  }
+
+  GetEmrDetails(id){
+    this.emrService.GetEmrDetails(id).subscribe(res=>{
       this.profiledetails= res.Data;
       console.log(this.profiledetails);
+      console.log(this.profiledetails.PatientEmrdocuments.length);
       
     })
   }
